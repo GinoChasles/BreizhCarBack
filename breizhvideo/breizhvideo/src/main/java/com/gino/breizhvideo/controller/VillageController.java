887954d306c05f895570cb0eab30020f56011f34
ginochasles <ginochasles@gmail.com>
2020-12-21 14:09:35 +0100

    création service villageimpl et villageservice

--- breizhvideo/breizhvideo/src/main/java/com/gino/breizhvideo/controller/VillageController.java
+++ breizhvideo/breizhvideo/src/main/java/com/gino/breizhvideo/controller/VillageController.java
@@ -1,22 +1,64 @@
 package com.gino.breizhvideo.controller;
 
 import com.gino.breizhvideo.model.Village;
+import com.gino.breizhvideo.service.VillageService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/breizhvideo")
 public class VillageController {
-        @CrossOrigin
-        @GetMapping("/village/nantes")
-        ResponseEntity<Village> getVillageNantes() {
-            Village nantes = new Village();
-            nantes.setName("Nantes");
-            nantes.setZipCode("44300");
-            return ResponseEntity.ok().body(nantes);
-        }
 
+    @Autowired
+    VillageService service;
+
+    @CrossOrigin
+    @GetMapping("/villages")
+    public ResponseEntity<List<Village>> getAllVillage(@RequestParam(value="search", defaultValue="") String search) {
+    List<Village> listVillage;
+    try{
+        listVillage = service.findAll(search);
+    } catch (Exception e) {
+        return ResponseEntity.notFound().build();
+    }
+    return ResponseEntity.ok().body(listVillage);
+    }
+
+    @CrossOrigin
+    @GetMapping("/villages/{id}")
+    ResponseEntity<Village> getVillageById(@PathVariable(value="id") long id) {
+        Optional<Village> village = service.findById(id);
+        if (village.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(village.get());
+    }
+    @CrossOrigin
+    @PostMapping("/villages")
+    ResponseEntity<Village> addVillage(@RequestBody Village village){
+        return ResponseEntity.ok().body(service.insert(village));
+    }
+    @CrossOrigin
+    @PutMapping("/villages/{id}")
+    ResponseEntity<Village> updateVillagee(@PathVariable(value="id") long id, @RequestBody Village village){
+    Village updatedVillage = service.update(id, village);
+    if(updatedVillage == null)
+        return ResponseEntity.notFound().build();
+
+    return ResponseEntity.ok().body(updatedVillage);
+    }
+    @CrossOrigin
+    @DeleteMapping("/villages/{id}")
+    ResponseEntity<Village> deleteVillage(@PathVariable(value="id") long id){
+    Optional<Village> village = service.findById(id);
+    if(village.isEmpty())
+        return ResponseEntity.notFound().build();
+
+    service.delete(village.get().getId());
+    return ResponseEntity.accepted().build();
+    }
 }
